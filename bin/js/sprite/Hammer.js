7466e0b810af001a9c1b5c8f2c994bbab564b878
cheneyweb <457299596@qq.com>
2018-07-05 00:33:15 +0800

    发布

--- bin/js/sprite/Hammer.js
+++ bin/js/sprite/Hammer.js
@@ -9,7 +9,7 @@ class Hammer extends ui.HammerUI {
     // 开始使用
     start() {
         this.visible = true
-        // Laya.Mouse.hide()
+        Laya.Mouse.hide()
         Laya.stage.on(Laya.Event.MOUSE_DOWN, this, this.onMouseDown)
         Laya.stage.on(Laya.Event.MOUSE_MOVE, this, this.onMouseMove)
         this.onMouseMove()
